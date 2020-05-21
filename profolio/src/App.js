dd4d77a730cf886c95d5cd2d9430f046e307762c
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-05-21 16:02:03 -0700

    set up the project on in different repo

---
+++ profolio/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1></h1>
+        <nav className="top">
+          <a>About</a>
+          <a>Projects</a>
+          <a>Experience</a>
+        </nav>
+      </header>
+      <section></section>
+      <footer>
+        <nav className="social">
+          <a>email</a>
+          <a>linkedin</a>
+          <a>twitch</a>
+          <a>email</a>
+          <a>email</a>
+        </nav>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
